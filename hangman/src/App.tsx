0ee1b6a7445e563e0d46f17fda7c2514568b811b
kamran quamar <kamranquamar2579@gmail.com>
2024-01-02 19:36:28 +0000

    hangmancodecomponent

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import './App.css';
-
+import React, { useState } from 'react';
+import WordList from './wordList.json';
+import { HangmanWord } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+import { HangmanDrawing } from './HangmanDrawing';
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(()=>{
+    return WordList[Math.floor(Math.random() * WordList.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  console.log(wordToGuess)
   return (
-    <div className="App">
-      <h1> Hello there</h1>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection : "column",
+        gap : "2rem",
+        margin : "0 auto",
+        alignItems : "center"
+
+    }}
+    >
+      <div style={{fontSize : "2rem", alignItems : "center"}}> win or Loose
+      </div>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
       </div>
   );
 }
